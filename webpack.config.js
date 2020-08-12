f3528133c03b32857bcec429649bbd8107d7a4ad
Marcos Rangel <marcos.junior@talkdesk.com>
2020-08-12 17:47:45 +0100

    Move index.html

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebPackPlugin({
-			template: "./src/index.html",
+			template: "./index.html",
 			filename: "./index.html",
 		}),
 		new MiniCssExtractPlugin({
